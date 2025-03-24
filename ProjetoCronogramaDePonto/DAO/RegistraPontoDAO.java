15c142e409785c266c4cbec28114674d8e1044f0
João Lucas Sorda de Almeida <joaolucasdealmeida1@gmail.com>
2025-03-24 17:06:22 -0300

    Update RegistraPontoDAO.java

---
+++ ProjetoCronogramaDePonto/DAO/RegistraPontoDAO.java
@@ -0,0 +1,35 @@
+package DAO;
+
+import Modelo.RegistraPonto;
+import Modelo.Funcionario;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class RegistraPontoDAO {
+    private static List<RegistroPonto> registros = new ArrayList<>();
+    private static int proximoId = 1;
+    
+    public void salvar(RegistroPonto registro){
+        if(registro.getId() == 0 ){
+            registro.setId(proximoId++);
+        }
+        registro.add(registro);
+    }
+    public RegistroPonto buscarRegistroAberto(Funcionario funcionario){
+        return registros.stream()
+        .filter( r -> r.getfuncionario().equals(funcionario) && r.getHoraSaida() == null)
+        .findFirst()
+        .orElse(null);
+    }
+    public List<RegistroPonto> listarPorFuncionario(Funcionario funcionario){
+        List<RegistroPonto> resultado = new ArrayList<>();
+        for(RegistroPonto r : registros){
+            if(r.getfuncionario().equals(funcionario)){
+                resultado.add(r);
+            }
+        }
+        return resultado;
+    }
+    
+}
